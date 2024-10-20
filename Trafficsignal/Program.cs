5d720b3cf2376d6ce07b63848b979dd27aab14d7
Dhml32 <92448556+Dhml32@users.noreply.github.com>
2024-10-20 19:05:28 +0300

    Add files via upload

---
+++ Trafficsignal/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+string Trafficsignal = Console.ReadLine();
+
+   if (Trafficsignal == "red" || Trafficsignal == "yellow" || Trafficsignal == "green")
+
+     {
+    
+
+          switch (Trafficsignal)
+          {
+            case "red":
+            Console.WriteLine("stop");
+              break; 
+
+           case "yellow":
+            Console.WriteLine("Get ready to stop ");
+            break; 
+
+              case "green":
+              Console.WriteLine("go");
+              break;   
+
+              default:
+              Console.WriteLine("Invaid traffic light color ");
+               break;
+
+   }   
+
+
+   }
+
